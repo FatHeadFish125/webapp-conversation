b69508c5e5a904a74cc3eafa91ea71e51722f90b
FatHeadFish125 <139837971+FatHeadFish125@users.noreply.github.com>
2023-07-21 16:06:52 +0700

    Update index.ts
    添加api_url

--- config/index.ts
+++ config/index.ts
@@ -1,7 +1,7 @@
 import type { AppInfo } from '@/types/app'
 export const APP_ID = '645e0a59-7d47-4e87-9a81-2d0203d0072f'
 export const API_KEY = 'app-cpbbezEmQa3QHLqqM8Kkhxf2'
-export const API_URL = ''
+export const API_URL = 'https://api.dify.ai/v1'
 export const APP_INFO: AppInfo = {
   title: 'Chat APP',
   description: '',
